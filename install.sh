031d2d2d507560fc1d4f2bd973ac8bcef7a63661
Olav van Schie <olavschie@hotmail.com>
2017-05-02 16:26:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/ovanschie/venom-cli/raw/master/venom.phar
+curl https://github.com/ovanschie/venom-cli/raw/master/venom.phar
 chmod +x venom.phar
 sudo mv venom.phar /usr/local/bin/venom
 venom --version

b7c1ff995a9a9fcc5bb7aeb5264b544ba5fea74d
Olav van Schie <olavschie@hotmail.com>
2017-06-23 11:08:31 +0200

    rename

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/ovanschie/venom-cli/master/venom.phar -O
-chmod +x venom.phar
-sudo mv venom.phar /usr/local/bin/venom
+curl https://raw.githubusercontent.com/ovanschie/venom-cli/master/venom-cli.phar -O
+chmod +x venom-cli.phar
+sudo mv venom-cli.phar /usr/local/bin/venom
 venom --version

8008196d048b5f0e303967722d05db1419df34d5
Olav van Schie <olavschie@hotmail.com>
2017-07-12 22:43:50 +0200

    Remove all lines

--- src/RemoveCommand.php
+++ src/RemoveCommand.php
@@ -10,6 +10,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class RemoveCommand extends Command
 {
+    protected $output;
+
+    /**
+     *
+     */
     public function configure()
     {
         $this->setName('remove')
@@ -17,15 +22,28 @@ class RemoveCommand extends Command
                 ->addArgument('domain', InputArgument::REQUIRED, 'Domain');
     }
 
+
     /**
      * @param InputInterface  $input
      * @param OutputInterface $output
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $host = new HostsFile();
+        $this->output = $output;
 
-        $domains = explode(',', $input->getArgument('domain'));
+        if ($input->getArgument('domain') == '*') {
+            return $this->removeAll();
+        }
+
+        return $this->remove(explode(',', $input->getArgument('domain')));
+    }
+
+    /**
+     * @param $domains
+     */
+    protected function remove($domains)
+    {
+        $host = new HostsFile();
 
         foreach ($domains as $domain) {
             $host->removeLine($domain);
@@ -33,6 +51,24 @@ class RemoveCommand extends Command
 
         $host->save();
 
-        $output->writeln(sprintf('Removed: %s', $input->getArgument('domain')));
+        $this->output->writeln(sprintf('Removed: %s', implode(' ', $domains)));
+    }
+
+    /**
+     *
+     */
+    protected function removeAll()
+    {
+        $host = new HostsFile();
+
+        foreach ($host->getLines() as $line) {
+            if ($line['domain'] != 'localhost' && $line['domain'] != 'broadcasthost') {
+                $host->removeLine($line['domain']);
+            }
+        }
+
+        $host->save();
+
+        $this->output->writeln('Removed all entries');
     }
 }
